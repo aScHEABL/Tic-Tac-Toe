608dfee5fa66b22873672ef1c54a739ed9d6259e
aScHEABL <natothun@gmail.com>
2022-07-20 23:21:00 +0800

    feat: mapDOM function WIP

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
 (function game () {
     console.log(`Hello World!`);
+    let mapDOM = (() => {
+        let gridID = `#grid-`;
+        const gridArray = [];
+        const restartButton = document.querySelector(`#restart-button`);
+        for (let i = 1; i < 10; i++) {
+            gridArray.push(gridID + i);
+        };
+        console.log(gridArray);
+    })();
+
     
+    let playerX = {};
+    let playerO = {};
+    let gameLogic = () => {
+
+    }
 })();
