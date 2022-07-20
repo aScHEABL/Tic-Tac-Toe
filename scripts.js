16075dc6cc7e1376da5c50af72242e5fdb519b57
aScHEABL <natothun@gmail.com>
2022-07-21 01:48:05 +0800

    feat: addEventListener to grids and button

--- scripts.js
+++ scripts.js
@@ -1,25 +1,35 @@
-(function game () {
+(function () {
     console.log(`Hello World!`);
     // target DOM elements
-    let gridID = `grid-`;
-    const gridArray = [];
-    const restartButton_DOM = document.querySelector(`#restart-button`);
-    for (let i = 0; i < 9; i++) {
-        gridArray.push(gridID + i);
-    };
-    const grids_DOM = gridArray.map((grid) => document.querySelector(`#${grid}`));
-    
-    // addEventListener
-    restartButton_DOM.addEventListener(`click`, () => {
-        console.log(`Restart button has been clicked!`);
-    });
-    grids_DOM.forEach((grid, index) => {
-        grid.addEventListener(`click`, () => {
-            console.log(`${gridArray[index]} has been clicked!`);
-        })
-    });
 
-    let 
+    let targetDOM = () => {
+        let gridID = `grid-`
+        const restartButton_DOM = document.querySelector(`#restart-button`);
+        const gridArray = [];
+        for (let i = 0; i < 9; i++) {
+            gridArray.push(gridID + i);
+        };
+        const grids_DOM = gridArray.map((grid) => document.querySelector(`#${grid}`));
+        return {restartButton_DOM, grids_DOM, gridArray};
+    };
+    // addEventListener
+
+    let addEventListener = (() => {
+        targetDOM().restartButton_DOM.addEventListener(`click`, () => {
+            console.log(`Restart button has been clicked!`);
+        })
+        targetDOM().grids_DOM.forEach((grid, index) => {
+            grid.addEventListener(`click`, () => {
+                console.log(`${targetDOM().gridArray[index]} has been clicked!`);
+            });
+        });
+    })();
+
+    // let cyclePlayerTurn = () => {
+    //     const turnArray = ["X", "O"];
+    //     let count = 0;
+
+    // };
 
     let playerX = {};
     let playerO = {};
