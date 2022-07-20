81c6e62b2e94d489b7beefd5721e80fee80b17e3
aScHEABL <natothun@gmail.com>
2022-07-21 00:38:09 +0800

    feat: addEventListener

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 (function game () {
     console.log(`Hello World!`);
-    let targetDOM = (() => {
-        let gridID = `#grid-`;
-        const gridArray = [];
-        const restartButton = document.querySelector(`#restart-button`);
-        for (let i = 1; i < 10; i++) {
-            gridArray.push(gridID + i);
-        };
-        console.log(gridArray);
-        return {restartButton};
-    })();
+    // target DOM elements
+    let gridID = `grid-`;
+    const gridArray = [];
+    const restartButton_DOM = document.querySelector(`#restart-button`);
+    for (let i = 0; i < 9; i++) {
+        gridArray.push(gridID + i);
+    };
+    const grids_DOM = gridArray.map((grid) => document.querySelector(`#${grid}`));
+    
+    // addEventListener
+    restartButton_DOM.addEventListener(`click`, () => {
+        console.log(`Restart button has been clicked!`);
+    });
+    grids_DOM.forEach((grid, index) => {
+        grid.addEventListener(`click`, () => {
+            console.log(`${gridArray[index]} has been clicked!`);
+        })
+    });
 
     let playerX = {};
     let playerO = {};
