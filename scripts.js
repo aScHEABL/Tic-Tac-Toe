62657f77acc13cd24a5cf8d1ec2ea17d2d8fae22
aScHEABL <natothun@gmail.com>
2022-07-22 03:10:02 +0800

    feat: target h2-title DOM and update accordingly

--- scripts.js
+++ scripts.js
@@ -19,16 +19,22 @@
     // game logic
     
     let gameLogic = (grid, index) => {
+
+
         // get the turn of function cyclePlayerTurn
         switch (cyclePlayerTurn()()) {
             // playerX's turn
             case 0:
                 grid.textContent = `X`;
                 console.log(`Player X's turn!`);
+                targetDOM().playerXTitle.style.display = `block`;
+                targetDOM().playerOTitle.style.display = `none`;
                 break;
             case 1:
                 grid.textContent = `O`;
                 console.log(`Player O's turn!`);
+                targetDOM().playerXTitle.style.display = `none`;
+                targetDOM().playerOTitle.style.display = `block`;
                 break;
         };
     };
@@ -43,7 +49,9 @@
             gridArray.push(gridID + i);
         };
         const grids_DOM = gridArray.map((grid) => document.querySelector(`#${grid}`));
-        return {restartButton_DOM, grids_DOM, gridArray};
+        const playerXTitle = document.getElementById(`player-x-title`);
+        const playerOTitle = document.getElementById(`player-o-title`);
+        return {restartButton_DOM, grids_DOM, gridArray, playerXTitle, playerOTitle};
     };
 
     // addEventListener
