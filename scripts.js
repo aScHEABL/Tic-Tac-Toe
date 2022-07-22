9317de9678cdea58dcc2c0e01ddf8cb8556cc375
aScHEABL <natothun@gmail.com>
2022-07-22 18:49:46 +0800

    fix: h2-title does not respond to player

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-(function () {
+(function game () {
     console.log(`Hello World!`);
+    //player array
+    const playerX = [];
+    const playerO = [];    
 
     // cycle player's turn
-
-    let playerX = [];
-    let playerO = [];
 
     let count = 0;
     const cyclePlayerTurn = () => {
@@ -16,8 +16,23 @@
 
     // game logic
     
-    let gameLogic = (grid, index) => {
+    const gameLogic = (grid, index) => {    
+        const sortCallBackFn = (a, b) => a - b;
+        // Win scenario
+        // const win0 = [0, 1, 2];
+        // const win1 = [3, 4, 5];
+        // const win2 = [6, 7, 8];
+        // const win3 = [0, 3, 6];
+        // const win4 = [1, 4, 7];
+        // const win5 = [2, 5 ,8];
+        // const win6 = [0, 4, 8];
+        // const win7 = [6, 4, 2];
+        const win = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]];
 
+        win.forEach((winScenario) => {
+            if (playerX.includes(winScenario) === true) console.log(`Player X won!`);
+            if (playerO.includes(winScenario) === true) console.log(`Player O won!`);
+        });
 
         // get the turn of function cyclePlayerTurn
         switch (cyclePlayerTurn()) {
@@ -25,19 +40,21 @@
             case 0:
                 grid.textContent = `X`;
                 playerX.push(index);
+                playerX.sort(sortCallBackFn);
                 console.log(`Player X's array: ${playerX}`);
-                // console.log(`Player X's turn!`);
-                targetDOM().playerXTitle.style.display = `block`;
-                targetDOM().playerOTitle.style.display = `none`;
-                break;
-            case 1:
-                grid.textContent = `O`;
-                playerO.push(index);
-                console.log(`Player O's array: ${playerO}`);
                 // console.log(`Player O's turn!`);
                 targetDOM().playerXTitle.style.display = `none`;
                 targetDOM().playerOTitle.style.display = `block`;
                 break;
+            case 1:
+                grid.textContent = `O`;
+                playerO.push(index);
+                playerO.sort(sortCallBackFn);
+                console.log(`Player X's array: ${playerO}`);
+                // console.log(`Player X's turn!`);
+                targetDOM().playerXTitle.style.display = `block`;
+                targetDOM().playerOTitle.style.display = `none`;
+                break;    
         };
     };
 
@@ -59,7 +76,7 @@
     // addEventListener
 
     let addEventListener = (() => {
-        let eventListenerCallBack = (grid, index) => {
+        let eventListenerCallBackFn = (grid, index) => {
             grid.addEventListener(`click`, () => {
                 console.log(`${targetDOM().gridArray[index]} has been clicked!`);
                 // if  player clicked the same grid again, exit the function
@@ -73,7 +90,7 @@
         targetDOM().restartButton_DOM.addEventListener(`click`, () => {
             console.log(`Restart button has been clicked!`);
         })
-        targetDOM().grids_DOM.forEach(eventListenerCallBack);
+        targetDOM().grids_DOM.forEach(eventListenerCallBackFn);
         
     })();
     
